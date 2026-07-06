85b1b6f06eeed1767281ce0d3c113ae890c33635
localhaos <li.anonimes@gmail.com>
2026-07-06 09:23:10 +0200

    Add close tabs left menu action

--- bin/Addons/extensions/close-tabs-right/service_worker.js
+++ bin/Addons/extensions/close-tabs-right/service_worker.js
@@ -1,12 +1,19 @@
 "use strict";
 
-const MENU_ID = "close-tabs-right";
+const MENU_CLOSE_LEFT = "close-tabs-left";
+const MENU_CLOSE_RIGHT = "close-tabs-right";
 
 function setupMenus() {
   chrome.contextMenus.removeAll(() => {
     chrome.contextMenus.create({
-      id: MENU_ID,
-      title: "Close tabs to the right",
+      id: MENU_CLOSE_LEFT,
+      title: "Zamknij karty po lewej",
+      contexts: ["tab", "action"]
+    });
+
+    chrome.contextMenus.create({
+      id: MENU_CLOSE_RIGHT,
+      title: "Zamknij karty po prawej",
       contexts: ["tab", "action"]
     });
   });
@@ -15,6 +22,22 @@ function setupMenus() {
 function getActiveTab(callback) {
   chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
     callback(tabs && tabs.length ? tabs[0] : null);
+  });
+}
+
+function closeTabsBefore(anchorTab) {
+  if (!anchorTab || anchorTab.id === undefined || anchorTab.windowId === undefined || anchorTab.index === undefined) {
+    return;
+  }
+
+  chrome.tabs.query({ windowId: anchorTab.windowId }, (tabs) => {
+    const tabIds = tabs
+      .filter((tab) => !tab.pinned && tab.id !== undefined && tab.index < anchorTab.index)
+      .map((tab) => tab.id);
+
+    if (tabIds.length) {
+      chrome.tabs.remove(tabIds);
+    }
   });
 }
 
@@ -34,6 +57,15 @@ function closeTabsAfter(anchorTab) {
   });
 }
 
+function closeTabsToLeft(tab) {
+  if (tab && tab.id !== undefined) {
+    closeTabsBefore(tab);
+    return;
+  }
+
+  getActiveTab(closeTabsBefore);
+}
+
 function closeTabsToRight(tab) {
   if (tab && tab.id !== undefined) {
     closeTabsAfter(tab);
@@ -46,7 +78,9 @@ function closeTabsToRight(tab) {
 chrome.runtime.onInstalled.addListener(setupMenus);
 chrome.runtime.onStartup.addListener(setupMenus);
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === MENU_ID) {
+  if (info.menuItemId === MENU_CLOSE_LEFT) {
+    closeTabsToLeft(tab);
+  } else if (info.menuItemId === MENU_CLOSE_RIGHT) {
     closeTabsToRight(tab);
   }
 });
