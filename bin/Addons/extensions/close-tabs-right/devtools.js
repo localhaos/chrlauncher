c847be3fe0e08ed55956c6641b443edbd792c8a0
localhaos <li.anonimes@gmail.com>
2026-07-06 19:22:17 +0200

    Disable Lag Guard panel on Facebook DevTools sessions

--- bin/Addons/extensions/close-tabs-right/devtools.js
+++ bin/Addons/extensions/close-tabs-right/devtools.js
@@ -1,8 +1,31 @@
 "use strict";
 
-chrome.devtools.panels.create(
-  "Lag Guard",
-  "",
-  "devtools_panel.html",
-  () => {}
-);
+const DISABLED_HOST_SUFFIXES = [
+  "facebook.com",
+  "fbcdn.net",
+  "messenger.com"
+];
+
+function isDisabledHost(hostname) {
+  const host = String(hostname || "").toLowerCase();
+  return DISABLED_HOST_SUFFIXES.some((suffix) => host === suffix || host.endsWith("." + suffix));
+}
+
+function createLagGuardPanel() {
+  chrome.devtools.panels.create(
+    "Lag Guard",
+    "",
+    "devtools_panel.html",
+    () => {}
+  );
+}
+
+try {
+  chrome.devtools.inspectedWindow.eval("location.hostname", { useContentScriptContext: false }, (hostname, exceptionInfo) => {
+    if (exceptionInfo || !isDisabledHost(hostname)) {
+      createLagGuardPanel();
+    }
+  });
+} catch (_) {
+  createLagGuardPanel();
+}
