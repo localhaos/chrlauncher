d36f1a895b02b7713911e01815eedfb650f027fe
localhaos <li.anonimes@gmail.com>
2026-07-06 10:18:56 +0200

    Make taskbar profile patch whitespace-safe

--- tools/prepare-taskbar-profile.ps1
+++ tools/prepare-taskbar-profile.ps1
@@ -23,9 +23,11 @@ $text = $text.Replace(
     'if (pbi->is_hasprofiledir)'
 )
 
-$text = $text.Replace(
-    'if (pbi->is_hasurls)' + "`r`n" + "\t" + '{' + "`r`n" + "\t\t" + 'profile_argument = _app_get_profile_argument (pbi);',
-    'if (pbi->is_hasurls || _app_config_getboolean (L"ChromiumUseLastProfileOnTaskbarLaunch", TRUE))' + "`r`n" + "\t" + '{' + "`r`n" + "\t\t" + 'profile_argument = _app_get_profile_argument (pbi);'
+$text = [regex]::Replace(
+    $text,
+    'if\s*\(pbi->is_hasurls\)\s*\{\s*profile_argument\s*=\s*_app_get_profile_argument\s*\(pbi\);',
+    "if (pbi->is_hasurls || _app_config_getboolean (L`"ChromiumUseLastProfileOnTaskbarLaunch`", TRUE))`r`n`t{`r`n`t`tprofile_argument = _app_get_profile_argument (pbi);",
+    [System.Text.RegularExpressions.RegexOptions]::Singleline
 )
 
 if ($text -notmatch 'ChromiumUseLastProfileOnTaskbarLaunch') {
